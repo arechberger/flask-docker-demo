58315d384d91aed0566c4cca57b997d3efac8fb1
Albert Rechberger <albert.rechberger@gmail.com>
2019-05-17 16:21:46 +0200

    add hostname info

--- app/main.py
+++ app/main.py
@@ -1,6 +1,15 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def hello_world():
-    return 'Hello, World! This is version: 0.0.2'
+    return "Hello, World! This is version: 0.0.3"
+
+
+@app.route("/hostname")
+def hostname():
+    HOSTNAME = os.getenv("HOSTNAME", "unknown")
+    return f"You are on host {HOSTNAME}"
--- app/tests.py
+++ app/tests.py
@@ -1,14 +1,34 @@
 import main
 import unittest
+from unittest import mock
+
 
 class FlaskrTestCase(unittest.TestCase):
-
     def setUp(self):
         self.app = main.app.test_client()
 
     def test_home_page(self):
-        home_page = self.app.get('/')
-        assert home_page.data.decode('utf-8') == 'Hello, World! This is version: 0.0.2'
+        home_page = self.app.get("/")
+        self.assertEqual(
+            home_page.data.decode("utf-8"), "Hello, World! This is version: 0.0.3"
+        )
 
-if __name__ == '__main__':
+    def test_host_name_base(self):
+        host_name = self.app.get("/hostname")
+        self.assertTrue(
+            host_name.data.decode("utf-8").startswith("You are on host"),
+            msg=host_name.data.decode("utf-8"),
+        )
+
+    @mock.patch("os.getenv", return_value="TESTHOST")
+    def test_hostname_full(self, mocked_getenv):
+        host_name = self.app.get("/hostname")
+        self.assertEqual(
+            host_name.data.decode("utf-8"),
+            "You are on host TESTHOST",
+            msg=host_name.data.decode("utf-8"),
+        )
+
+
+if __name__ == "__main__":
     unittest.main()
